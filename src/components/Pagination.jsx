8de82b7a2ad465905d0444e86835a7e79785c83d
Jonathan David <jhernadezcorrea@gmail.com>
2023-08-15 21:05:53 -0500

    mistake solved

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Pagination = ({pages, setCurrentPage, currentpage}) => {
+const Pagination = ({pages, setCurrentPage, currentPage}) => {
   return (
-    <ul className='flex gap-4 flex-wrap justify-center'>
+    <ul className='flex gap-4 flex-wrap justify-center  mt-10 mb-10 mx-5'>
         {
-            pages.map((page) => (<li className={`rounded h-12 cursor-pointer hover:scale-110 transition-transform false ${currentpage === page && "bg-blue-500"}`} onClick={() => setCurrentPage(page)} key={page}>{page}</li>))
+            pages.map((page) => (<li className={`bg-green-500 rounded-sm m-auto p-1  font-bold items-center justify-center   cursor-pointer hover:scale-110 transition-transform false ${currentPage === page && "bg-blue-500"}`} onClick={() => setCurrentPage(page)} key={page}>{page}</li>))
         }
       
     </ul>
